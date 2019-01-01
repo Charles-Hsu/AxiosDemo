6af49173fbdaa37e33f5b50768d2fc6273486d9d
Charles Hsu <hulu2020@gmail.com>
2019-01-01 20:47:34 +0800

    再一次練習#4

--- js/script3.js
+++ js/script3.js
@@ -1,17 +1,11 @@
-$('.clearResult').click(function() {
-  $('#getResult1').html('');
-  $('#getResult2').html('');
-  $('#postResult').html('');
-});
-
 $('#getRequest1').click(function() {
   axios
     .get('http://jsonplaceholder.typicode.com/todos')
     .then(function(response) {
-      $('#getResult1').html(getSuccessHTML(response));
+      $('#getResult1').html(generateSuccessHTMLOutput(response));
     })
     .catch(function(error) {
-      $('#getResult1').html(getFailHTML(error));
+      $('#getResult1').html(generateErrorHTMLOutput(error));
     });
 });
 
@@ -23,10 +17,10 @@ $('#getRequest2').click(function() {
       }
     })
     .then(function(response) {
-      $('#getResult2').html(getSuccessHTML(response));
+      $('#getResult2').html(generateSuccessHTMLOutput(response));
     })
     .catch(function(error) {
-      $('#getResult2').html(getFailHTML(error));
+      $('#getResult2').html(generateErrorHTMLOutput(error));
     });
 });
 
@@ -35,17 +29,23 @@ $('#postRequest').click(function() {
     .post('http://jsonplaceholder.typicode.com/todos', {
       userId: '1',
       title: $('#todoTitle').val(),
-      completed: true
+      completed: false
     })
     .then(function(response) {
-      $('#postResult').html(getSuccessHTML(response));
+      $('#postResult').html(generateSuccessHTMLOutput(response));
     })
     .catch(function(error) {
-      $('#postResult').html(getFailHTML(error));
+      $('#postResult').html(generateErrorHTMLOutput(error));
     });
 });
 
-function getSuccessHTML(response) {
+$('.clearResult').click(function() {
+  $('#getResult1').html('');
+  $('#getResult2').html('');
+  $('#postResult').html('');
+});
+
+function generateSuccessHTMLOutput(response) {
   return (
     '<h4>Result</h4>' +
     '<h5>Status:</h5>' +
@@ -56,25 +56,35 @@ function getSuccessHTML(response) {
     '</pre>' +
     '<h5>Headers</h5>' +
     '<pre>' +
-    JSON.stringify(response.headers, null, '  ') +
+    JSON.stringify(response.headers, null, '\t') +
     '</pre>' +
     '<h5>Data</h5>' +
     '<pre>' +
-    JSON.stringify(response.data, null, '  ') +
+    JSON.stringify(response.data, null, '\t') +
     '</pre>'
   );
 }
 
-function getFailHTML(error) {
+function generateErrorHTMLOutput(error) {
   return (
     '<h4>Result</h4>' +
+    '<h5>Message:</h5>' +
+    '<pre>' +
+    error.message +
+    '</pre>' +
     '<h5>Status:</h5>' +
     '<pre>' +
     error.response.status +
+    ' ' +
+    error.response.statusText +
     '</pre>' +
-    '<h5>Message</h5>' +
+    '<h5>Headers</h5>' +
     '<pre>' +
-    error.message +
+    JSON.stringify(error.response.headers, null, '\t') +
+    '</pre>' +
+    '<h5>Data</h5>' +
+    '<pre>' +
+    JSON.stringify(error.response.data, null, '\t') +
     '</pre>'
   );
 }
